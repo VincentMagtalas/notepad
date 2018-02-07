5438dfe9594d8b27aa1b35429a9b343417bd73d3
VincentMagtalas <joseph.magtalas@gmail.com>
2018-02-07 11:21:50 +0800

    Used Generic Views in serialize

--- serialize/urls.py
+++ serialize/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    url(r'^serialize/$', views.snippet_list, name='serialize'),
-    url(r'^serialize/(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippets_detail'),
+    url(r'^serialize/$', views.GenericNoteList.as_view(), name='serialize'),
+    url(r'^serialize/(?P<pk>[0-9]+)/$', views.GenericNoteDetail.as_view(), name='snippets_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- serialize/views.py
+++ serialize/views.py
@@ -1,24 +1,101 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from rest_framework import generics
 
 from .serializer import NoteSerializer
 from notes.models import Note
 
-@csrf_exempt
-def snippet_list(request):
+#function based API View
+@api_view(['GET','POST'])
+def note_list(request,format=None):
     """
     List all code serialize, or create a new snippet.
     """
     if request.method == 'GET':
         snippets = Note.objects.all()
         serializer = NoteSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def snippet_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def note_detail(request, pk):
     note = get_object_or_404(Note, pk=pk)
     if request.method == 'GET':
-        snippets = Note.objects.filter(pk=pk)
+        snippets = Note.objects.filter(pk=note.id)
         serializer = NoteSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = NoteSerializer(Note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#Class-based API View
+class NoteList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        snippets = Note.objects.all()
+        serializer = NoteSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class NoteDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return Note.objects.get(pk=pk)
+        except Note.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = NoteSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = NoteSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#Generic Class-based API View
+class GenericNoteList(generics.ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+class GenericNoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
